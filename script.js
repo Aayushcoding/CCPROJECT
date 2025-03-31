087e2a3e4064fc7d6954a1c78673ea66bedb154a
AayushChavan <142100021+Aayushcoding@users.noreply.github.com>
2025-03-31 13:52:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function showMessage() {
     let messageElement = document.getElementById("message");
     
     if (messageElement.innerText === "") {
-        messageElement.innerText = "Hi, I am Jaywant Awhad";
+        messageElement.innerText = "Hi, I am Jaywant Avhad";
     } else {
         messageElement.innerText = "";
     }
